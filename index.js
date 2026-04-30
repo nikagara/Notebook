79163bff0f3c7f6dacd309744dbff9f5d6dc5f9d
eve <nikitakoltamenkov@proton.me>
2026-04-30 18:25:24 +0300

    сохранения в локальном хранилище

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const input = document.getElementById('note-title');
 const button = document.querySelector('.create');
+let notes = []
 
 button.addEventListener('click', function(button) {
     const text = input.value;
     const li = document.createElement('li');
     li.className = 'note-item'
     li.textContent = text;
+    notes.push(text)
+    localStorage.setItem('notes', JSON.stringify(notes))
 
     const btn = document.createElement('button')
     btn.className = 'delete-btn'
@@ -17,4 +20,25 @@ button.addEventListener('click', function(button) {
 })
 
     document.getElementById('notes-list').appendChild(li);
+    input.value = ''
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    // читаем из localStorage и отрисовываем заметки
+    const savedNotes = JSON.parse(localStorage.getItem('notes').
+    foreEach(function(loadNote) {
+    const lisave = document.createElement('li');
+    lisave.className = 'note-item'
+    lisave.textContent = loadNote;
+
+    const btn = document.createElement('button')
+    btn.className = 'delete-btn'
+    btn.textContent = 'Удалить'
+    li.appendChild(btn)
+
+    btn.addEventListener('click', function() {
+    li.remove()
+})
+    document.getElementById('notes-list').appendChild(li);
+    }))
 })
